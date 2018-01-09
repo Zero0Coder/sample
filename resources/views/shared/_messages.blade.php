ae902b71c9d4f54149d8ca05c281d2be04987ce5
vagrant <vagrant@homestead>
2018-01-09 02:44:53 +0000

    Finish user signup

---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,11 @@
+@foreach(['danger', 'warning', 'success', 'inof'] as $msg)
+    @if(session()->has($msg))
+        <div class="flash-message">
+        	<p class="alert alert-{{ $msg }}">
+        		{{ session()->get($msg) }}
+        	</p>
+        </div>
+    @endif
+@endforeach
+
+
